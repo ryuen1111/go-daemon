eaab59d7d184996c2bd344a12f01602000755ad5
Naoki Enda <enda@casy.co.jp>
2016-07-22 07:29:25 +0900

    add glide and daemon

--- main.go
+++ main.go
@@ -1,3 +1,31 @@
 package main
 
+import (
+	"log"
 
+	"github.com/sevlyar/go-daemon"
+	"github.com/ryuen1111/go-daemon/app"
+)
+
+func main() {
+	cntxt := &daemon.Context{
+		PidFileName: "pid",
+		PidFilePerm: 0644,
+		LogFileName: "log",
+		LogFilePerm: 0640,
+		WorkDir:     "./",
+		Umask:       027,
+		Args:        []string{"[go-daemon]"},
+	}
+
+	d, err := cntxt.Reborn()
+	if err != nil {
+		log.Println(err)
+	}
+	if d != nil {
+		return
+	}
+	defer cntxt.Release()
+
+	app.MessageLoop()
+}
